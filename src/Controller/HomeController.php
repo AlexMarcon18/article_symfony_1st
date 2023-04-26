b72bc772e57df6f86c2ae5813278a5fbda447e10
Marcon <marcon.alex@outlook.fr>
2023-04-26 16:19:07 +0200

    update gitignore

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/about', name: '_about')]
+    public function about(): Response
+    {
+        return $this->render('about/index.html.twig', [
+            'controller_name' => 'AboutController',
+        ]);
+    }
+}
