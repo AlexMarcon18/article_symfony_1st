92bec726bc820e96026d68daa94cb710cb677aba
Alex Marcon <marcon.alex@outlook.fr>
2023-05-26 09:40:40 +0200

    update contenus

--- src/DataFixtures/CategoriesFixtures.php
+++ src/DataFixtures/CategoriesFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Categories;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,10 +10,17 @@ class CategoriesFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-         $category = new categories();
-         $category-> setName('categories');
+         $category = new Categories();
+         $category-> setName('config');
+         $category-> setName('first');
+         $category-> setName('structure');
+         $category-> setName('sbd');
+         $category-> setName('biblio');
 
-         $manager->persist($category);
+
+
+
+        $manager->persist($category);
 
          $manager->flush();
     }
